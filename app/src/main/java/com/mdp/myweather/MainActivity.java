ecd7b7cc46c3a87324cce26efb790c9cfdaaaa00
MuhammadDody2002 <92378148+MuhammadDody2002@users.noreply.github.com>
2022-09-29 15:00:57 +0700

    update

--- app/src/main/java/com/mdp/myweather/MainActivity.java
+++ app/src/main/java/com/mdp/myweather/MainActivity.java
@@ -1,38 +1,73 @@
 package com.mdp.myweather;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.mdp.myweather.databinding.ActivityMainBinding;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@RequiresApi(api = Build.VERSION_CODES.N)
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
     private ActivityMainBinding binding;
     private GoogleMap nMap;
     private List<Lokasi> restaurantList = new ArrayList<>();
     private List<Lokasi> hospitalList = new ArrayList<>();
+    private ActivityResultLauncher<String[]> locationPermissionRequest = registerForActivityResult(new ActivityResultContracts.RequestMultiplePermissions(), result -> {
+        Boolean fineLocationGranted = result.getOrDefault(
+                Manifest.permission.ACCESS_FINE_LOCATION, false);
+        Boolean coarseLocationGranted = result.getOrDefault(
+                Manifest.permission.ACCESS_COARSE_LOCATION, false);
+        if (fineLocationGranted != null && fineLocationGranted) {
 
+        } else if (coarseLocationGranted != null && coarseLocationGranted) {
+
+        } else {
+            // no location access granted
+        }
+    });
+    private FusedLocationProviderClient fusedLocationProviderClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        locationPermissionRequest.launch(new String[]{
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+        });
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+
         restaurantList.add(new Lokasi("Belido Restaurant", new LatLng(-2.9544493129715845, 104.74733438938732)));
         restaurantList.add(new Lokasi("Pecel Lele Shakila", new LatLng(-2.9536211329513455, 104.750980246199)));
         restaurantList.add(new Lokasi("RM Pindang Raja Pegagan", new LatLng(-2.9539454876745976, 104.75165688415095)));
@@ -51,7 +86,43 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         hospitalList.add(new Lokasi("RS Muhammad Hoesin", new LatLng(-2.966201144158307, 104.74912754539834)));
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-        mapFragment.getMapAsync (this);
+        mapFragment.getMapAsync(this);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        fusedLocationProviderClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location!= null){
+                    double latitude = location.getLatitude();
+                    double longitude = location.getLongitude();
+                    LatLng latLnglast = new LatLng(latitude, longitude);
+
+                    nMap.clear();
+                    nMap.addMarker(new MarkerOptions()
+                            .position(latLnglast)
+                            .title("I'm Here")
+                            .snippet("Help Me!!"))
+                            .showInfoWindow();
+
+                    nMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLnglast, 17));
+                    nMap.addCircle(new CircleOptions()
+                            .center(latLnglast)
+                            .radius(100)
+                            .strokeColor(Color.TRANSPARENT)
+                            .fillColor(R.color.purple_500));
+                }
+
+            }
+        });
 
         binding.fabRestaurant.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -90,6 +161,12 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         LatLng latlinguser = new LatLng(-2.962740, 104.740023);
         nMap.addMarker (new MarkerOptions().position(latlinguser).title("lokasi saya"));
         nMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latlinguser, 19));
+
+        nMap.addCircle(new CircleOptions()
+                .center(latlinguser)
+                .radius(100)
+                .strokeColor(Color.TRANSPARENT)
+                .fillColor(R.color.purple_500));
 
     }
 }
